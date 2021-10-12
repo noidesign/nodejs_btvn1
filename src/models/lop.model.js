538fe1aaab904e7055be12b2ac53e7209c43a8eb
oppa2711 <trangianoi2711@gmail.com>
2021-10-12 10:24:01 +0700

    first commit

---
+++ src/models/lop.model.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const { toJSON, paginate } = require('./plugins');
+
+const lopSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    numberStudent: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// add plugin that converts mongoose to json
+lopSchema.plugin(toJSON);
+lopSchema.plugin(paginate);
+
+/**
+ * @typedef Lop
+ */
+const Lop = mongoose.model('Lop', lopSchema);
+
+module.exports = Lop;
--- src/validations/index.js
+++ src/validations/index.js
@@ -1,2 +1,4 @@
 module.exports.authValidation = require('./auth.validation');
 module.exports.userValidation = require('./user.validation');
+module.exports.sinhvienValidation = require('./sinhvien.validation');
+module.exports.lopValidation = require('./lop.validation');
